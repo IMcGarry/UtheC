8937abb771622816f8a769948e02164bef88be46
IMcGarry <117388427+IMcGarry@users.noreply.github.com>
2023-10-19 11:28:25 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-<<<<<<< HEAD
+
 
 public class Main {
 
     public static BookListener bl;
     public static void main(String[] args) {
         bl =new BookListener("");
-=======
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
->>>>>>> main
+
         new Webpage();
     }
 }
