2fc873992b45915121208f673fd97261b2e8215d
Issie <isobel.mcgarry@gmail.com>
2023-10-19 12:07:20 +0100

    merge

--- src/BookListener.java
+++ src/BookListener.java
@@ -1,72 +1,98 @@
 import javax.swing.*;
 import java.awt.event.*;
-<<<<<<< HEAD
-=======
+
 import java.sql.SQLException;
->>>>>>> 5415c1b936c2c3d293bc7986a7d2996151ef9343
+
 
 public class BookListener implements MouseListener {
-    private String bookPath;
-    JDialog popUp = null;
-    public BookListener(String path){
-        this.bookPath = path;
-    }
+    public String bookPath;
 
-    private void createPopUp(String bookPath){
-        popUp = new JDialog();
-        popUp.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-        popUp.setLocationRelativeTo(null);
+    public class BookListener implements MouseListener {
+        private String bookPath;
+        JDialog popUp = null;
 
-        ImageIcon bookCover = new ImageIcon(bookPath);
-        JLabel bookCoverLabel = new JLabel(bookCover);
-
-        popUp.add(bookCoverLabel);
-        popUp.pack();
-        popUp.setVisible(true);
-
-        popUp.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosed(WindowEvent e) {
-                popUp = null;
-            }
-        });
-
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        if(popUp == null){
-            createPopUp(bookPath);
-<<<<<<< HEAD
-=======
-            DatabaseController databaseController = new DatabaseController();
-            try {
-                databaseController.GetBookDetails(bookPath);
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
->>>>>>> 5415c1b936c2c3d293bc7986a7d2996151ef9343
+        public BookListener(String path) {
+            this.bookPath = path;
         }
 
-    }
 
-    @Override
-    public void mousePressed(MouseEvent e) {
+        public void setBookPath(String s) {
+            bookPath = s;
+        }
 
-    }
+        public String getBookPath() {
+            return bookPath;
+        }
 
-    @Override
-    public void mouseReleased(MouseEvent e) {
 
-    }
+        private void createPopUp(String bookPath) {
+            popUp = new JDialog();
+            popUp.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+            popUp.setLocationRelativeTo(null);
 
-    @Override
-    public void mouseEntered(MouseEvent e) {
+            ImageIcon bookCover = new ImageIcon(bookPath);
+            JLabel bookCoverLabel = new JLabel(bookCover);
 
-    }
+            popUp.add(bookCoverLabel);
 
-    @Override
-    public void mouseExited(MouseEvent e) {
+            popUp.setAlwaysOnTop(true);
 
+            popUp.pack();
+            popUp.setVisible(true);
+
+            popUp.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosed(WindowEvent e) {
+                    popUp = null;
+                }
+            });
+
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+            Object obj = e.getSource();
+            if (obj instanceof BookLabelMaker) {
+                setBookPath(((BookLabelMaker) obj).bookPath);
+            }
+
+            if (popUp == null) {
+                createPopUp(bookPath);
+                DatabaseController databaseController = new DatabaseController();
+                try {
+                    databaseController.GetBookDetails(bookPath);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+
+                if (popUp == null) {
+                    createPopUp(bookPath);
+
+                }
+
+            }
+
+            @Override
+            public void mousePressed (MouseEvent e){
+
+            }
+
+            @Override
+            public void mouseReleased (MouseEvent e){
+
+            }
+
+            @Override
+            public void mouseEntered (MouseEvent e){
+
+            }
+
+            @Override
+            public void mouseExited (MouseEvent e){
+
+            }
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
-<<<<<<< HEAD
+
+
+public class Main {
+
+    public static BookListener bl;
+
+    public static void main(String[] args) {
+        bl = new BookListener("");
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-=======
->>>>>>> 5415c1b936c2c3d293bc7986a7d2996151ef9343
-public class Main {
-    public static void main(String[] args) {
-        new Webpage();
+        public class Main {
+            public static void main(String[] args) {
+
+                new Webpage();
+            }
+        }
     }
 }
